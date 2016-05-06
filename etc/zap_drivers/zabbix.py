e6c4b6c5645750dfb942664247ce48d9f5ad9ff3
Vladimir Ulogov <vladimir.ulogov@zabbix.com>
2016-05-06 10:50:34 -0400

    Zabbix protocol driver implementation

--- etc/zap_drivers/zabbix.py
+++ etc/zap_drivers/zabbix.py
@@ -1,9 +1,21 @@
-__author__ = 'gandalf'
+__author__  =  'Vladimir Ulogov'
+__version__ = 'v0.1.0'
 
+import struct
 from ProtocolDriver import *
 
 class ZabbixProtocol(ProtocolDriver):
-    pass
+    def recv_data(self, data):
+        hdr = struct.unpack("ssssB", data[:5])
+        sig = "".join(list(hdr[:4]))
+        ver = list(hdr)[-1]
+        if sig != "ZBXD":
+            return data
+        p_len = struct.unpack("L", data[5:13])[0]
+        return data[13:p_len]
+    def send_data(self, data):
+        hdr="ZBXD"+struct.pack("B",1)+struct.pack("L",len(data)+1)
+        return hdr+data
 
 def main(env, logger, *args, **argv):
     return ProtocolDriverCreator("zabbix", env, logger, ZabbixProtocol, args, argv)
